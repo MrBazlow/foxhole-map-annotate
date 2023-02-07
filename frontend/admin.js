95b556e0866f32bbc5c522ec56294501f4545e9e
Mr Bazlow <67396813+MrBazlow@users.noreply.github.com>
2023-02-07 20:29:36 +0000

    More WIP design changes. Not functional.

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,4 +1,44 @@
+const { Sortable } = require('sortablejs');
+const { Tabs } = require('flowbite');
+
 addListeners(document)
+
+// Admin tabs
+const tabElements = [
+  {
+    id: 'general',
+    triggerEl: document.querySelector('#general-tab'),
+    targetEl: document.querySelector('#general')
+  },
+  {
+    id: 'discord',
+    triggerEl: document.querySelector('#discord-tab'),
+    targetEl: document.querySelector('#discord')
+  },
+  {
+    id: 'text',
+    triggerEl: document.querySelector('#text-tab'),
+    targetEl: document.querySelector('#text')
+  }
+]
+
+const tabOptions = {
+  defaultTabId: 'general',
+  activeClasses: 'bg-warden-600 active-tab',
+  inactiveClasses: 'inactive-tab'
+}
+
+const adminTabs = new Tabs(tabElements, tabOptions)
+
+// Sortable.js Link list drag and drop.
+const linkList = document.getElementById('linkList')
+const sortableLinkList = Sortable.create(linkList, {
+  handle: ".link-handle",
+  animation: 200,
+  easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+  onMove: (event) => (event.related.classList.contains('sortable'))
+})
+
 
 function addListeners(target) {
   const deleteRowButtons = target.getElementsByClassName('deleteRow')
